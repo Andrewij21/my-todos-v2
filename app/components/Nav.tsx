deaf133a53dfed4282d618f2666f24fe89ca1932
Andre Wijaya <andrewijaya988@gmail.com>
2024-10-03 11:43:07 +0700

    feat: add not complete auth

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,17 +1,32 @@
+// "use client";
 import { Button } from "@/components/ui/button";
+// import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { handleSignout } from "../actions/authActions";
+import { auth } from "@/auth";
 
-export default function Nav() {
+export default async function Nav() {
+  const session = await auth(); //gunakan ini di server component
+  // const { data: session } = useSession();
+  console.log({ session });
   return (
     <nav className="px-4 py-2 flex justify-between items-center shadow">
       <h1 className="text-3xl tracking-tight font-bold capitalize">
         <Link href="/">MY TODO</Link>
       </h1>
       <div className="flex items-center gap-x-4">
-        <Button variant={"outline"}>Sign in</Button>
-        <Button>Sign up</Button>
-        {/* <p>example@mail.com</p>
-        <Button variant="destructive">logout</Button> */}
+        {session ? (
+          <form action={handleSignout}>
+            <Button type="submit">Sign out</Button>
+          </form>
+        ) : (
+          <>
+            <Button variant={"outline"} asChild>
+              <Link href="/auth/signin">Sign in</Link>
+            </Button>
+            {/* <Button>Sign up</Button> */}
+          </>
+        )}
       </div>
     </nav>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SessionProvider } from "next-auth/react";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -25,13 +26,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Nav />
-        {children}
-      </body>
-    </html>
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Nav />
+          {children}
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
