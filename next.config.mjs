c67fe0008d65145e74aa10084098a97b889ae9af
Andre Wijaya <andrewijaya988@gmail.com>
2024-10-08 11:26:14 +0700

    ga tau final

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,19 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)", // Mengatur CORS untuk semua routes
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Mengizinkan semua domain (sesuaikan sesuai kebutuhan)
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Authorization",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
